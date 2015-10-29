7193187fa8c626816cbdbf890ff47482d1afb2c1
Frederic Luchting <frederic@luchting.de>
2015-10-29 21:45:31 +0100

    Initial Commit

---
+++ OliApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace OliApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ OliApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OliApi.WebApiApplication" Language="C#" %>
